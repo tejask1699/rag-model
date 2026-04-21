a205789b71af193f40d45dd79e39e1ef48c9952b
Tejas Kelaskar <90203396+tejask1699@users.noreply.github.com>
2026-04-22 01:00:09 +0530

    Update temp_check.js

--- temp_check.js
+++ temp_check.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_q9NxsotWbO0n@ep-aged-sky-am13qv8n-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: "",
 });
 
 async function checkSchema() {
